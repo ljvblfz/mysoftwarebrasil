3f305da9c8599462e41630ab861fc2d4bf002624
alexismoura@gmail.com <alexismoura@gmail.com@de68f9c9-cb92-f4be-4cd0-3cc118f4193e>
2012-02-07 01:14:59 +0000

    add validation ao projeto

---
+++ trunk/project/CSharp/ponto/PontoEncontro.Application/Validation/CheckIntegrit.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using PontoEncontro.Domain;
+
+namespace PontoEncontro.Application.Validate
+{
+    /// <summary>
+    ///  Validation de verificação de integridade
+    /// </summary>
+    public class CheckIntegrit : IValidate
+    {
+        /// <summary>
+        ///  Messagens de erro
+        /// </summary>
+        public string ErrorMessage
+        {
+            get
+            {
+                return "Existe vinculo do registro em outros cadastros";
+            }
+        }
+
+        /// <summary>
+        ///  Indica se o objeto e valido
+        ///  TRUE : valido
+        ///  FALSE : não é valido
+        /// </summary>
+        public bool IsValid { get; set; }
+
+        /// <summary>
+        ///  Construtor
+        /// </summary>
+        public CheckIntegrit(object Entity)
+        {
+            var repositoryType = typeof(GenericRepository<>).MakeGenericType(Entity.GetType());
+            object repo = Activator.CreateInstance(repositoryType);
+            var isIntegrity = repo.GetType().GetMethod("CheckedIntegity").Invoke(repo, new object[] { Entity});
+
+            // Se existe retrição a validação e falsa
+            if ((bool)isIntegrity)
+                this.IsValid = false;
+        }
+    }
+}
---
+++ trunk/project/CSharp/ponto/PontoEncontro.Application/Validation/IValidate.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace PontoEncontro.Application.Validate
+{
+    /// <summary>
+    /// Provides the means to check if the validity of the current state of an <see langword="object"/>.
+    /// </summary>
+    /// <seealso cref="NotifyValidatableBase"/>
+    /// <seealso cref="DataErrorInfoValidatableBase"/>
+    /// <seealso cref="ValidatableBase"/>
+    public interface IValidate
+    {
+        /// <summary>
+        /// Gets a <see langword="bool"/> indicating if the current state is valid.
+        /// </summary>
+        bool IsValid { get; set; }
+
+        /// <summary>
+        /// Gets a <see see="{T}"/> of <see langword="string"/>s that contain all the error messages.
+        /// </summary>
+        string ErrorMessage { get;}
+    }
+}
