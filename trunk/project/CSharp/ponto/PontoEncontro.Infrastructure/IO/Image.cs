7c3b60393b1c3c4858b5b1d9413494a3d9f2594b
alexismoura@gmail.com <alexismoura@gmail.com@de68f9c9-cb92-f4be-4cd0-3cc118f4193e>
2012-02-21 18:15:00 +0000

    add galeria de fotos

--- trunk/project/CSharp/ponto/PontoEncontro.Infrastructure/IO/Image.cs
+++ trunk/project/CSharp/ponto/PontoEncontro.Infrastructure/IO/Image.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Web;
+using System.Web.Mvc;
 
 namespace PontoEncontro.Infrastructure.IO
 {
-    public class Image //: System.Net.Mime.MediaTypeNames.Image
+    /// <summary>
+    /// Classe de manipulação de imagem
+    /// </summary>
+    public class Image
     {
+        /// <summary>
+        ///  Retorna a imagem em base64
+        /// </summary>
+        /// <param name="path">string caminho da imgem</param>
+        /// <returns>string imagem em base64</returns>
         public static string GetBase64Image(string path)
         {
             FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read);
@@ -15,6 +25,38 @@ namespace PontoEncontro.Infrastructure.IO
             fileStream.Read(data, 0, data.Length);
 
             return Convert.ToBase64String(data);
-        } 
+        }
+
+        /// <summary>
+        ///  Exclusão logica da imagem
+        /// </summary>
+        /// <param name="fileName">caminho da imagem</param>
+        public static bool DeleteImage(string fileName)
+        {
+            try
+            {
+                var file = new FileInfo(fileName);
+                file.Delete();
+                return true;
+            }
+            catch (IOException ex)
+            {
+                if (ex.InnerException != null)
+                    throw new Exception(ex.InnerException.Message, ex);
+                throw new Exception(ex.Message, ex);
+            }
+        }
+
+        /// <summary>
+        ///  Converte o caminho fisico para caminho virtual
+        /// </summary>
+        /// <param name="physicalFilePath">caminho fisico</param>
+        /// <returns>caminho virtual</returns>
+        public static string GetVirtualPath(string physicalFilePath)
+        {
+            return physicalFilePath
+                        .Replace(HttpRuntime.AppDomainAppPath, "/")
+                        .Replace(Path.DirectorySeparatorChar, '/');
+        }
     }
 }
