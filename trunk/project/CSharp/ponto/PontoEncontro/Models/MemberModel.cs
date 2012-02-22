8bb280ec6c486723e805f4f7ac0a2613b3bcccd5
alexismoura@gmail.com <alexismoura@gmail.com@de68f9c9-cb92-f4be-4cd0-3cc118f4193e>
2012-02-22 00:44:26 +0000

    busca de usuario

---
+++ trunk/project/CSharp/ponto/PontoEncontro/Models/MemberModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Web.Mvc;
+using System.Web.Security;
+using System.Collections;
+
+namespace PontoEncontro.Models
+{
+    public class MemberModel
+    {
+        [UIHint("DropDown")]
+        [Display(Name = "Estado")]
+        public IEnumerable<SelectListItem> Idestado { get; set; }
+
+        [UIHint("DropDown")]
+        [Display(Name = "Cidade")]
+        public IEnumerable<SelectListItem> Idcidade { get; set; }
+
+        [UIHint("ListMembros")]
+        public IList membros { get; set; }
+
+        public MemberModel()
+        {
+        }
+    }
+}
