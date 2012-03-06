f7734d5e90810b0fdb3413030b8efbe3af621ee6
alexismoura <alexismoura@gmail.com>
2012-03-06 01:50:02 +0000

    atualização do nome do projeto

--- project/CSharp/ponto/PontoEncontro.Domain/NHibernateHelper/SessionFactoryHelper.cs
+++ project/CSharp/ponto/PontoEncontro.Domain/NHibernateHelper/SessionFactoryHelper.cs
@@ -26,7 +26,33 @@ namespace PontoEncontro.Domain
             var cfg = new Configuration().Configure();
             var config = Fluently.Configure(cfg);
             sessionFactory = config
-                            .Mappings(MappingsAssembly)
+                            //.Mappings(MappingsAssembly)
+                            .Mappings(x => x.FluentMappings.Add<ActionMap>())
+                            .Mappings(x => x.FluentMappings.Add<AssociacaoMap>())
+                            .Mappings(x => x.FluentMappings.Add<AssociadoMap>())
+                            .Mappings(x => x.FluentMappings.Add<BairroMap>())
+                            .Mappings(x => x.FluentMappings.Add<CabeloMap>())
+                            .Mappings(x => x.FluentMappings.Add<CidadeMap>())
+                            .Mappings(x => x.FluentMappings.Add<ContatoMap>())
+                            .Mappings(x => x.FluentMappings.Add<ControllerMap>())
+                            .Mappings(x => x.FluentMappings.Add<EnderecoMap>())
+                            .Mappings(x => x.FluentMappings.Add<EstadoCivilMap>())
+                            .Mappings(x => x.FluentMappings.Add<EstadoMap>())
+                            .Mappings(x => x.FluentMappings.Add<EtiniaMap>())
+                            .Mappings(x => x.FluentMappings.Add<FotoMap>())
+                            .Mappings(x => x.FluentMappings.Add<InteresseMap>())
+                            .Mappings(x => x.FluentMappings.Add<MembroMap>())
+                            .Mappings(x => x.FluentMappings.Add<MenuMap>())
+                            .Mappings(x => x.FluentMappings.Add<OlhoMap>())
+                            .Mappings(x => x.FluentMappings.Add<PerfilMap>())
+                            .Mappings(x => x.FluentMappings.Add<PermissaoRoleMap>())
+                            .Mappings(x => x.FluentMappings.Add<PermissoesMap>())
+                            .Mappings(x => x.FluentMappings.Add<PessoaMap>())
+                            .Mappings(x => x.FluentMappings.Add<RoleMap>())
+                            .Mappings(x => x.FluentMappings.Add<SexoMap>())
+                            .Mappings(x => x.FluentMappings.Add<TipoAssociacaoMap>())
+                            .Mappings(x => x.FluentMappings.Add<TipoContatoMap>())
+                            .Mappings(x => x.FluentMappings.Add<TipoInteresseMap>())
                             .BuildSessionFactory();
 
 			return sessionFactory;
